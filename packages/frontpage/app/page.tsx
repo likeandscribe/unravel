dae3b4ef15e8da371d3fa3ebdb61127fb21084ea
Tom Sherman <the.tomsherman@gmail.com>
2024-06-11 01:17:58 +0100

    WIP

--- packages/frontpage/app/page.tsx
+++ packages/frontpage/app/page.tsx
@@ -1,7 +1,19 @@
+import { getSession } from "@/lib/auth";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
+      <Link href="/sign-in">Sign in</Link>
       <div>Hello world!</div>
+      <User />
     </main>
   );
+}
+
+async function User() {
+  const session = await getSession();
+  if (!session) return null;
+  console.log(session);
+  return <pre>{JSON.stringify(session.user)}</pre>;
 }
