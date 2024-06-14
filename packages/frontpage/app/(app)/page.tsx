bac1cc74fc6d4def0c7afe72e9beaa945aa62ed8
Tom Sherman <the.tomsherman@gmail.com>
2024-06-14 15:23:58 +0100

    Add no store

--- packages/frontpage/app/(app)/page.tsx
+++ packages/frontpage/app/(app)/page.tsx
@@ -1,7 +1,9 @@
 import { getFrontpagePosts } from "@/lib/data";
 import { PostCard } from "./_components/post-card";
+import { unstable_noStore } from "next/cache";
 
 export default async function Home() {
+  unstable_noStore();
   const posts = await getFrontpagePosts();
   return (
     <div className="space-y-6">
@@ -20,42 +22,3 @@ export default async function Home() {
     </div>
   );
 }
-
-const tempPosts = {
-  "1": {
-    id: "1",
-    title: "Introducing the new Tailwind CSS v3.0",
-    url: "https://tailwindcss.com",
-    votes: 42,
-    author: "@john.example.com",
-    createdAt: new Date("2024-06-13T15:10:44.335Z"),
-    commentCount: 4,
-  },
-  "2": {
-    id: "1",
-    title: "React 18 is here! What's new?",
-    url: "https://reactjs.org",
-    votes: 27,
-    author: "@jane.example.com",
-    createdAt: new Date("2024-06-11T04:10:44.335Z"),
-    commentCount: 0,
-  },
-  "3": {
-    id: "1",
-    title: "The future of web development with WebAssembly",
-    url: "https://webassembly.org",
-    votes: 19,
-    author: "@alex.example.com",
-    createdAt: new Date("2024-06-10T07:10:44.335Z"),
-    commentCount: 23,
-  },
-  "4": {
-    id: "1",
-    title: "The rise of no-code and low-code platforms",
-    url: "https://nocode.com",
-    votes: 12,
-    author: "@sarah.example.com",
-    createdAt: new Date("2024-06-08T07:10:44.335Z"),
-    commentCount: 0,
-  },
-};
