7ede1fb1ab44d1afe5263df030622f6afb8dc8d4
Jake Saterlay <41985955+JakeSaterlay@users.noreply.github.com>
2024-06-15 21:49:05 +0100

    removed forwardRefs from component lib (#71)

--- packages/frontpage/lib/components/ui/button.tsx
+++ packages/frontpage/lib/components/ui/button.tsx
@@ -40,20 +40,25 @@ export interface ButtonProps
   asChild?: boolean;
 }
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, children, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button";
-    return (
-      <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
-        ref={ref}
-        {...props}
-      >
-        <Slottable>{children}</Slottable>
-      </Comp>
-    );
-  },
-);
+const Button = ({
+  className,
+  variant,
+  size,
+  asChild = false,
+  children,
+  ...props
+}: ButtonProps) => {
+  const Comp = asChild ? Slot : "button";
+  return (
+    <Comp
+      className={cn(buttonVariants({ variant, size, className }))}
+      {...props}
+    >
+      <Slottable>{children}</Slottable>
+    </Comp>
+  );
+};
+
 Button.displayName = "Button";
 
 export { Button, buttonVariants };
