647bb6d3abf78e40c9c8444fe92aa859994c6fb2
Tom Sherman <the.tomsherman@gmail.com>
2024-06-18 13:12:53 +0100

    Try forcing static

--- packages/frontpage/app/(app)/page.tsx
+++ packages/frontpage/app/(app)/page.tsx
@@ -1,6 +1,7 @@
 import { Home } from "./_components/home-page";
 
 export const revalidate = 900; // 15 minutes
+export const dynamic = "force-static";
 
 export default function HomePage() {
   return <Home />;
