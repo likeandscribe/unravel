de31aedf73c4e80e7376cf73c7c054437563f2ab
Timothy <1695613+timothyis@users.noreply.github.com>
2024-06-22 22:29:08 +0100

    Improve background colours (#91)

--- packages/frontpage/app/(app)/layout.tsx
+++ packages/frontpage/app/(app)/layout.tsx
@@ -32,7 +32,7 @@ export default async function Layout({
       </div>
 
       {session && (
-        <div className="flex justify-between items-center bg-slate-100 dark:bg-slate-900 dark:text-slate-300 p-4 mb-4 gap-2 rounded-md">
+        <div className="flex justify-between items-center bg-slate-100 dark:bg-slate-900/50 dark:text-slate-300 p-4 mb-4 gap-2 rounded-md">
           <span>
             {isInBeta ? (
               <>
