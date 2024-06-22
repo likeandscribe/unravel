c40a44de34a3cd1698845920f7cd6ea2c24d6b4f
Tom Sherman <the.tomsherman@gmail.com>
2024-06-22 19:06:36 +0100

    Fix type error

--- packages/frontpage/lib/components/ui/tooltip.tsx
+++ packages/frontpage/lib/components/ui/tooltip.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import * as React from "react";
 import * as TooltipPrimitive from "@radix-ui/react-tooltip";
 
 import { cn } from "@/lib/utils";
 
-const TooltipProvider = TooltipPrimitive.Provider;
+const TooltipProvider: (props: {
+  children: React.ReactNode;
+}) => React.ReactNode = TooltipPrimitive.Provider;
 
 const Tooltip = TooltipPrimitive.Root;
 
