f4dd0a695bd6c7f0ab4bc1d691e6252bb07ff242
Jake Saterlay <jakesaterlay@hotmail.co.uk>
2024-06-23 00:16:15 +0100

    changed bg colour of text area dark mode

--- packages/frontpage/lib/components/ui/textarea.tsx
+++ packages/frontpage/lib/components/ui/textarea.tsx
@@ -12,7 +12,7 @@ const Textarea = ({
   return (
     <textarea
       className={cn(
-        "flex min-h-[60px] w-full rounded-md border border-input resize-none rounded-2xl bg-transparent px-3 py-3 text-sm shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+        "flex min-h-[60px] w-full rounded-md border border-input resize-none rounded-2xl bg-transparent px-3 py-3 text-sm shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 dark:bg-slate-900/30",
         className,
       )}
       {...props}
