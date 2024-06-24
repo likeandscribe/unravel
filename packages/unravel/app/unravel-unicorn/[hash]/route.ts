138a40b19f44730d018139f24a3c9a9313429568
Tom Sherman <the.tomsherman@gmail.com>
2024-06-24 20:42:12 +0100

    Add unicorn embed to homepage (#95)
    * Add unicorn embed to homepage
    
    * Make script sync
    
    * Cache for long time

---
+++ packages/unravel/app/unravel-unicorn/[hash]/route.ts
@@ -0,0 +1,28 @@
+import { notFound } from "next/navigation";
+import unicorn from "./unravel-unicorn.json";
+import crypto from "node:crypto";
+
+const HASH = crypto
+  .createHash("md5")
+  .update(JSON.stringify(unicorn))
+  .digest("hex");
+
+export const dynamic = "force-static";
+
+type Params = { hash: string };
+
+export function generateStaticParams(): Params[] {
+  console.log("Generated unravel-unicorn hash:", HASH);
+
+  return [{ hash: HASH }];
+}
+
+export function GET(_: Request, { params }: { params: Params }) {
+  console.log(params);
+  if (params.hash !== HASH) notFound();
+  return Response.json(unicorn, {
+    headers: {
+      "Cache-Control": "public, max-age=31536000, immutable",
+    },
+  });
+}
