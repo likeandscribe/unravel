be5ec8effcacf45d4dbbb7877e000f9c0159b575
Tom Sherman <the.tomsherman@gmail.com>
2024-06-24 14:39:08 +0100

    Render blog posts (#94)
    * Render blog posts
    
    * Render title and pretty URL
    
    * Styles for li and blockquotes

--- packages/unravel/app/blog/page.tsx
+++ packages/unravel/app/blog/page.tsx
@@ -1,13 +1,25 @@
+import Link from "next/link";
 import { listBlogs } from "./blog-data";
+
+const dateFormatter = new Intl.DateTimeFormat("en-US", {
+  dateStyle: "medium",
+});
 
 export default async function Blog() {
   const blogList = await listBlogs();
   return (
     <>
-      <h1>Blog</h1>
-      <ul>
+      <h1 className="text-4xl mb-8">Unravel Blog</h1>
+      <ul className="flex flex-col space-y-6">
         {blogList.records.map((blog) => (
-          <li key={blog.cid}>{blog.value.content}</li>
+          <li key={blog.cid}>
+            <Link href={`/blog/${blog.slug}`}>
+              <article className="shadow-sm rounded-lg p-4 bg-white dark:bg-slate-900">
+                <h2 className="text-xl">{blog.value.title}</h2>
+                <p>{dateFormatter.format(blog.value.createdAt)}</p>
+              </article>
+            </Link>
+          </li>
         ))}
       </ul>
     </>
