d4293f237b2aab5e55d2ffd4ded879b9728dbd6d
Tom Sherman <the.tomsherman@gmail.com>
2024-06-25 23:26:34 +0100

    Add see all comments link

--- packages/frontpage/app/(app)/post/[postRkey]/[commentRkey]/page.tsx
+++ packages/frontpage/app/(app)/post/[postRkey]/[commentRkey]/page.tsx
@@ -2,6 +2,7 @@ import { getCommentWithChildren } from "@/lib/data/db/comment";
 import { notFound } from "next/navigation";
 import { Comment } from "../_commentServer";
 import { getPost } from "@/lib/data/db/post";
+import Link from "next/link";
 
 type Params = {
   commentRkey: string;
@@ -19,17 +20,27 @@ export default async function CommentPage({ params }: { params: Params }) {
   }
 
   return (
-    <Comment
-      isUpvoted={false}
-      key={comment.id}
-      cid={comment.cid}
-      rkey={comment.rkey}
-      postRkey={params.postRkey}
-      authorDid={comment.authorDid}
-      createdAt={comment.createdAt}
-      id={comment.id}
-      comment={comment.body}
-      childComments={comment.children}
-    />
+    <>
+      <div className="flex justify-end">
+        <Link
+          href={`/post/${params.postRkey}`}
+          className="font-medium text-indigo-600 hover:text-indigo-500 dark:text-indigo-400 dark:hover:text-indigo-300"
+        >
+          See all comments
+        </Link>
+      </div>
+      <Comment
+        isUpvoted={false}
+        key={comment.id}
+        cid={comment.cid}
+        rkey={comment.rkey}
+        postRkey={params.postRkey}
+        authorDid={comment.authorDid}
+        createdAt={comment.createdAt}
+        id={comment.id}
+        comment={comment.body}
+        childComments={comment.children}
+      />
+    </>
   );
 }
