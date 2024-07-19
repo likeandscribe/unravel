2cac1f0e3345bcc892b238f6c43a2d05d83c455e
Tom Sherman <the.tomsherman@gmail.com>
2024-07-20 00:16:44 +0100

    Ignore errors while prototyping

--- packages/frontpage/next.config.mjs
+++ packages/frontpage/next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // FIXME: Remove this when we're closer to production
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   experimental: {
     reactCompiler: true,
   },
