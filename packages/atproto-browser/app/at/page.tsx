e7ba3a98720e492d6ce1ccd36798db8b18554f3f
Tom Sherman <the.tomsherman@gmail.com>
2024-08-12 21:37:32 +0100

    Show did history (#113)
    * Show did history as JSON
    
    * Trigger build
    
    * Trigger build
    
    * Show history as human readable
    
    * Handle create and tombstone

--- packages/atproto-browser/app/at/page.tsx
+++ packages/atproto-browser/app/at/page.tsx
@@ -7,13 +7,15 @@ import {
 } from "@atproto/identity";
 import { AtUri, isValidHandle } from "@atproto/syntax";
 import { isDid } from "@atproto/did";
-import { cache, Suspense } from "react";
+import { cache, Fragment, Suspense } from "react";
 import Link from "next/link";
 import { AtBlob } from "./_lib/at-blob";
 import { CollectionItems } from "./_lib/collection";
 import { SWRConfig } from "swr";
 import { listRecords } from "@/lib/atproto";
 import { verifyRecords } from "@atproto/repo";
+import { ErrorBoundary } from "react-error-boundary";
+import { z } from "zod";
 
 const didResolver = new DidResolver({});
 const resolveDid = cache((did: string) => didResolver.resolve(did));
@@ -57,7 +59,14 @@ export default async function AtPage({
         <h1>
           {handle} ({didDocument.id})
         </h1>
-        <Author did={didStr} />
+        <DidSummary did={didStr} />
+
+        <Suspense fallback={<p>Loading history...</p>}>
+          <ErrorBoundary fallback={<p>Failed to fetch history.</p>}>
+            <h2>History</h2>
+            <DidHistory did={didStr} />
+          </ErrorBoundary>
+        </Suspense>
       </>
     );
   }
@@ -117,11 +126,10 @@ export default async function AtPage({
     <div>
       <details>
         <summary>
-          Author: {handle} (<Link href={`/at?u=at://${didStr}/`}>{didStr}</Link>
-          )
+          Author: {handle} (<Link href={`/at?u=at://${didStr}`}>{didStr}</Link>)
         </summary>
         <Suspense>
-          <Author did={didStr} />
+          <DidSummary did={didStr} />
         </Suspense>
       </details>
       <h2>
@@ -196,7 +204,27 @@ async function RecordVerificationBadge({
   }
 }
 
-async function Author({ did }: { did: string }) {
+async function DidSummary({ did }: { did: string }) {
+  return (
+    <>
+      <ErrorBoundary
+        fallback={<div>Failed to fetch collections for {did}.</div>}
+      >
+        <h2>Collections</h2>
+        <DidCollections did={did} />
+      </ErrorBoundary>
+      <h2>DID Doc</h2>
+      <DidDoc did={did} />
+    </>
+  );
+}
+
+async function DidDoc({ did }: { did: string }) {
+  const didDocument = await resolveDid(did);
+  return <JSONValue data={didDocument as JSONType} repo={did} />;
+}
+
+async function DidCollections({ did }: { did: string }) {
   const didDocument = await resolveDid(did);
   if (!didDocument) {
     throw new Error(`Could not resolve DID: ${did}`);
@@ -216,11 +244,8 @@ async function Author({ did }: { did: string }) {
   });
 
   if (!response.ok) {
-    return (
-      <div>
-        Failed to fetch collections: {response.statusText}. URL:{" "}
-        {describeRepoUrl.toString()}
-      </div>
+    throw new Error(
+      `Failed to fetch collections: ${response.statusText}. URL: ${describeRepoUrl.toString()}`,
     );
   }
 
@@ -229,26 +254,307 @@ async function Author({ did }: { did: string }) {
   };
 
   return (
-    <>
-      <h2>Collections</h2>
-      <ul>
-        {collections.length === 0 ? (
-          <p>No collections.</p>
-        ) : (
-          collections.map((nsid) => {
-            const collectionUri = `at://${[did, nsid].join("/")}`;
+    <ul>
+      {collections.length === 0 ? (
+        <p>No collections.</p>
+      ) : (
+        collections.map((nsid) => {
+          const collectionUri = `at://${[did, nsid].join("/")}`;
 
-            return (
-              <li key={nsid}>
-                <Link href={`/at?u=${collectionUri}`}>{nsid}</Link>
-              </li>
-            );
+          return (
+            <li key={nsid}>
+              <Link href={`/at?u=${collectionUri}`}>{nsid}</Link>
+            </li>
+          );
+        })
+      )}
+    </ul>
+  );
+}
+
+const PlcLogAuditResponse = z.array(
+  z.object({
+    createdAt: z
+      .string()
+      .datetime()
+      .transform((x) => new Date(x)),
+    operation: z.union([
+      z.object({
+        type: z.literal("plc_operation"),
+        sig: z.string(),
+        prev: z.string().nullable(),
+        services: z.record(
+          z.object({
+            type: z.string(),
+            endpoint: z.string(),
+          }),
+        ),
+        alsoKnownAs: z.array(z.string()),
+        rotationKeys: z.array(z.string()),
+        verificationMethods: z.record(z.string()),
+      }),
+      z.object({
+        type: z.literal("create"),
+        signingKey: z.string(),
+        recoveryKey: z.string(),
+        handle: z.string(),
+        service: z.string(),
+      }),
+      z.object({
+        type: z.literal("plc_tombstone"),
+      }),
+    ]),
+  }),
+);
+
+const utcDateFormatter = new Intl.DateTimeFormat("en-US", {
+  dateStyle: "medium",
+  timeStyle: "short",
+  timeZone: "UTC",
+});
+
+function isNotNull<T>(x: T | null): x is T {
+  return x !== null;
+}
+
+async function DidHistory({ did }: { did: string }) {
+  const response = await fetch(`https://plc.directory/${did}/log/audit`);
+  if (!response.ok) {
+    throw new Error(`Failed to fetch history: ${response.statusText}`);
+  }
+
+  const auditLog = PlcLogAuditResponse.parse(await response.json());
+
+  return (
+    <ol>
+      {auditLog.map((previous, index) => {
+        const previousOperation = previous.operation;
+        if (previousOperation.type !== "plc_operation") {
+          return (
+            // eslint-disable-next-line react/no-array-index-key
+            <li key={index}>
+              Change created at {utcDateFormatter.format(previous.createdAt)}{" "}
+              (UTC) of type &quot;{previousOperation.type}&quot;.
+            </li>
+          );
+        }
+        const entry = auditLog[index + 1];
+        if (!entry) {
+          return null;
+        }
+        const entryOperation = entry.operation;
+        if (entryOperation.type !== "plc_operation") {
+          return null;
+        }
+
+        const alsoKnownAsAdded = entryOperation.alsoKnownAs.filter(
+          (x) => !previousOperation.alsoKnownAs.includes(x),
+        );
+        const alsoKnownAsRemoved = previousOperation.alsoKnownAs.filter(
+          (x) => !entryOperation.alsoKnownAs.includes(x),
+        );
+
+        const servicesChanged = Object.entries(entryOperation.services)
+          .map(([id, service]) => {
+            const previousService = previousOperation.services[id];
+            if (!previousService) return null;
+            return {
+              id,
+              type:
+                service.type !== previousService.type
+                  ? {
+                      from: previousService.type,
+                      to: service.type,
+                    }
+                  : null,
+              endpoint:
+                service.endpoint !== previousService.endpoint
+                  ? {
+                      from: previousService.endpoint,
+                      to: service.endpoint,
+                    }
+                  : null,
+            };
           })
-        )}
-      </ul>
-      <h2>DID Doc</h2>
-      <JSONValue data={didDocument as JSONType} repo={did} />
-    </>
+          .filter(isNotNull);
+
+        const servicesAdded = Object.entries(entryOperation.services).filter(
+          ([id]) => !previousOperation.services[id],
+        );
+        const servicesRemoved = Object.entries(
+          previousOperation.services,
+        ).filter(([id]) => !entryOperation.services[id]);
+
+        const rotationKeysAdded = entryOperation.rotationKeys.filter(
+          (x) => !previousOperation.rotationKeys.includes(x),
+        );
+        const rotationKeysRemoved = previousOperation.rotationKeys.filter(
+          (x) => !entryOperation.rotationKeys.includes(x),
+        );
+
+        const verificationMethodsChanged = Object.entries(
+          entryOperation.verificationMethods,
+        )
+          .map(([id, key]) => {
+            const previousKey = previousOperation.verificationMethods[id];
+            if (!previousKey) return null;
+            if (key === previousKey) return null;
+            return {
+              id,
+              from: previousKey,
+              to: key,
+            };
+          })
+          .filter(isNotNull);
+        const verificationMethodsAdded = Object.entries(
+          entryOperation.verificationMethods,
+        ).filter(([id]) => !previousOperation.verificationMethods[id]);
+        const verificationMethodsRemoved = Object.entries(
+          previousOperation.verificationMethods,
+        ).filter(([id]) => !entryOperation.verificationMethods[id]);
+
+        return (
+          // eslint-disable-next-line react/no-array-index-key
+          <li key={index}>
+            <p>
+              Change created at {utcDateFormatter.format(entry.createdAt)} (UTC)
+            </p>
+            <ul>
+              {alsoKnownAsAdded.length === 1 &&
+              alsoKnownAsRemoved.length === 1 ? (
+                <li>
+                  Alias changed from{" "}
+                  <Link href={`/at?u=${alsoKnownAsRemoved[0]}`}>
+                    {alsoKnownAsRemoved[0]}
+                  </Link>{" "}
+                  to{" "}
+                  <Link href={`/at?u=${alsoKnownAsAdded[0]}`}>
+                    {alsoKnownAsAdded[0]}
+                  </Link>
+                </li>
+              ) : (
+                <>
+                  {alsoKnownAsAdded.length > 0 && (
+                    <li>
+                      Alias added:{" "}
+                      {alsoKnownAsAdded.flatMap((aka) => [
+                        <Link key={aka} href={`/at?u=${aka}`}>
+                          {aka}
+                        </Link>,
+                        ", ",
+                      ])}
+                    </li>
+                  )}
+                  {alsoKnownAsRemoved.length > 0 && (
+                    <li>
+                      Alias removed:{" "}
+                      {alsoKnownAsRemoved.flatMap((aka) => [
+                        <Link key={aka} href={`/at?u=at://${aka}`}>
+                          {aka}
+                        </Link>,
+                        ", ",
+                      ])}
+                    </li>
+                  )}
+                </>
+              )}
+              {servicesChanged.length > 0 &&
+                servicesChanged.map((service) => (
+                  <Fragment key={service.id}>
+                    {!!service.type && (
+                      <li key={service.id}>
+                        Service &quot;{service.id}&quot; changed type from
+                        &quot;
+                        {service.type.from}&quot; to &quot;{service.type.to}
+                        &quot;
+                      </li>
+                    )}
+                    {!!service.endpoint && (
+                      <li key={service.id}>
+                        Service &quot;{service.id}&quot; changed endpoint from{" "}
+                        <a href={service.endpoint.from}>
+                          {service.endpoint.from}
+                        </a>{" "}
+                        to{" "}
+                        <a href={service.endpoint.to}>{service.endpoint.to}</a>
+                      </li>
+                    )}
+                  </Fragment>
+                ))}
+              {servicesAdded.length > 0 && (
+                <li>
+                  Services added:{" "}
+                  {servicesAdded.flatMap(([id, service]) => [
+                    <Link key={id} href={service.endpoint}>
+                      {id} ({service.type})
+                    </Link>,
+                    ", ",
+                  ])}
+                </li>
+              )}
+              {servicesRemoved.length > 0 && (
+                <li>
+                  Services removed:{" "}
+                  {servicesRemoved.flatMap(([id, service]) => [
+                    <Link key={id} href={service.endpoint}>
+                      {id} ({service.type})
+                    </Link>,
+                    ", ",
+                  ])}
+                </li>
+              )}
+              {rotationKeysAdded.length > 0 && (
+                <li>
+                  Rotation keys added:{" "}
+                  {rotationKeysAdded.flatMap((key) => [
+                    <code key={key}>{key}</code>,
+                    ", ",
+                  ])}
+                </li>
+              )}
+              {rotationKeysRemoved.length > 0 && (
+                <li>
+                  Rotation keys removed:{" "}
+                  {rotationKeysRemoved.flatMap((key) => [
+                    <code key={key}>{key}</code>,
+                    ", ",
+                  ])}
+                </li>
+              )}
+              {verificationMethodsChanged.length > 0 &&
+                verificationMethodsChanged.map((method) => (
+                  <li key={method.id}>
+                    Verification method &quot;{method.id}&quot; changed from{" "}
+                    <code>{method.from}</code> to <code>{method.to}</code>
+                  </li>
+                ))}
+              {verificationMethodsAdded.length > 0 && (
+                <li>
+                  Verification methods added:{" "}
+                  {verificationMethodsAdded.flatMap(([id, key]) => [
+                    <Fragment key={id}>
+                      <code>{key}</code> (&quot;{id}&quot;)
+                    </Fragment>,
+                    ", ",
+                  ])}
+                </li>
+              )}
+              {verificationMethodsRemoved.length > 0 && (
+                <li>
+                  Verification methods removed:{" "}
+                  {verificationMethodsRemoved.flatMap(([id, key]) => [
+                    <Fragment key={id}>
+                      <code>{key}</code> (&quot;{id}&quot;)
+                    </Fragment>,
+                    ", ",
+                  ])}
+                </li>
+              )}
+            </ul>
+          </li>
+        );
+      })}
+    </ol>
   );
 }
 
