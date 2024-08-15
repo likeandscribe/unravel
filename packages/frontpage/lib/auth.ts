d7a98b5c421a7712d5c6b74d5d1b06cb0fd5d65f
Tom Sherman <the.tomsherman@gmail.com>
2024-08-15 21:30:39 +0100

    Client ID needs to be the client-metadata URL

--- packages/frontpage/lib/auth.ts
+++ packages/frontpage/lib/auth.ts
@@ -36,7 +36,9 @@ export const getClientConfig = cache(() => {
       : process.env.VERCEL_PROJECT_PRODUCTION_URL!;
 
   return {
-    client_id: `frontpage-${process.env.NODE_ENV === "production" ? "prod" : "nonprod"}`,
+    // Client ID is the URL of the client metadata
+    // This isn't immediately obvious and if you supply something else the PAR request will fail with a 400 "Invalid url" error. I had to traverse the atproto implementation to find out why!
+    client_id: `${appUrl}/oauth/client-metadata.json`,
     dpop_bound_access_tokens: true,
     application_type: "web",
     subject_type: "public",
