0b8bc954efa9686bf8a05bc5e93f16865ab94a10
Tom Sherman <the.tomsherman@gmail.com>
2024-08-28 20:17:45 +0100

    Also preload stream url

--- packages/atproto-browser/app/at/_lib/video-embed.tsx
+++ packages/atproto-browser/app/at/_lib/video-embed.tsx
@@ -19,22 +19,27 @@ export async function VideoEmbed({ cid, did }: VideoEmbedProps) {
 async function VideoEmbedInner({ cid, did }: VideoEmbedProps) {
   const rootUrl = `https://lumi.jazco.dev/watch/${did}/${cid}`;
   const source = `${rootUrl}/playlist.m3u8`;
+  const manifestText = await fetch(source, {
+    next: {
+      revalidate: 600,
+    },
+  }).then((res) => res.text());
 
-  const manifest = HLS.parse(
-    await fetch(source, {
-      next: {
-        revalidate: 600,
-      },
-    }).then((res) => res.text()),
-  );
+  const manifest = HLS.parse(manifestText);
 
-  // This is quite naive, but it's good enough for now
-  const sessionId = manifest.streamRenditions[0]?.uri.split("session_id=")[1];
+  const streamRenditionUri = manifest.streamRenditions[0]?.uri;
+  const sessionId = streamRenditionUri?.split("session_id=")[1];
 
-  if (sessionId) {
+  if (streamRenditionUri && sessionId) {
+    // This is quite naive, but it's good enough for now
     const sourceUrl = new URL(source);
     sourceUrl.searchParams.set("session_id", sessionId);
     preload(sourceUrl.toString(), {
+      as: "fetch",
+      crossOrigin: "anonymous",
+    });
+
+    preload(`${rootUrl}/${streamRenditionUri}`, {
       as: "fetch",
       crossOrigin: "anonymous",
     });
