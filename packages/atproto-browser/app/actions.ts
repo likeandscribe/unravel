936c3325d334ce9805c30e6d8251339a6d36479a
Tom Sherman <the.tomsherman@gmail.com>
2024-08-29 20:57:49 +0100

    Big ol' route restructure (#116)
    * Big ol' route restructure
    
    * Dont share params

--- packages/atproto-browser/app/actions.ts
+++ packages/atproto-browser/app/actions.ts
@@ -1,8 +1,10 @@
 "use server";
 
+import { getAtUriPath } from "@/lib/util";
+import { AtUri } from "@atproto/syntax";
 import { redirect } from "next/navigation";
 
 export async function navigateUri(_state: unknown, formData: FormData) {
-  const uri = formData.get("uri") as string;
-  redirect(`/at?u=${uri}`);
+  const uri = new AtUri(formData.get("uri") as string);
+  redirect(getAtUriPath(uri));
 }
