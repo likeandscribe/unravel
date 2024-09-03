d9a30ed800b9c2bb7254c9b58f10429359504e0a
Tom Sherman <the.tomsherman@gmail.com>
2024-09-03 15:06:31 +0100

    Add footer

--- packages/atproto-browser/app/page.tsx
+++ packages/atproto-browser/app/page.tsx
@@ -19,6 +19,19 @@ export default function Home() {
       <p>
         eg. <Link href={`/at/${EXAMPLE_PATH}`}>at://{EXAMPLE_PATH}</Link>
       </p>
+
+      <footer>
+        <p>
+          Developed by{" "}
+          <Link href="/at/tom-sherman.com//app.bsky.actor.profile/self">
+            @tom-sherman.com
+          </Link>
+          .{" "}
+          <a href="https://github.com/likeandscribe/unravel/tree/main/packages/atproto-browser">
+            Source code
+          </a>
+        </p>
+      </footer>
     </main>
   );
 }
