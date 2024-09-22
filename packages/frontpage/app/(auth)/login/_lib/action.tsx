24e6635620e36a0cbcd3dcd2574f1c97f8fb725c
Tom Sherman <the.tomsherman@gmail.com>
2024-09-22 21:42:13 +0100

    Drop @

--- packages/frontpage/app/(auth)/login/_lib/action.tsx
+++ packages/frontpage/app/(auth)/login/_lib/action.tsx
@@ -3,7 +3,7 @@ import { signIn } from "@/lib/auth-sign-in";
 
 export async function loginAction(_prevStart: unknown, formData: FormData) {
   const identifier = formData.get("identifier") as string;
-  const result = await signIn(identifier);
+  const result = await signIn(identifier.replace(/^@/, ""));
   if (result && "error" in result) {
     return {
       error: `An error occured while signing in (${result.error})`,
