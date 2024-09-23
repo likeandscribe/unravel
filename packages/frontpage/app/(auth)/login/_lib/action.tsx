9281dddb6fdd938d35cd92a9167d1d3465faec93
Tom Sherman <the.tomsherman@gmail.com>
2024-09-23 21:14:12 +0100

    Ensure handles are treated as case insensitive when signing in

--- packages/frontpage/app/(auth)/login/_lib/action.tsx
+++ packages/frontpage/app/(auth)/login/_lib/action.tsx
@@ -1,9 +1,18 @@
 "use server";
 import { signIn } from "@/lib/auth-sign-in";
+import { isValidHandle } from "@atproto/syntax";
 
 export async function loginAction(_prevStart: unknown, formData: FormData) {
   const identifier = formData.get("identifier") as string;
-  const result = await signIn(identifier.replace(/^@/, ""));
+  let handleOrDid = identifier;
+  // Sanitize only handles
+  if (
+    isValidHandle(identifier) ||
+    isValidHandle(identifier.replace(/^@/, ""))
+  ) {
+    handleOrDid = identifier.replace(/^@/, "").toLowerCase();
+  }
+  const result = await signIn(handleOrDid);
   if (result && "error" in result) {
     return {
       error: `An error occured while signing in (${result.error})`,
