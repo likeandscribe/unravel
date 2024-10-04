ee144574834aca795b03468600a105d3f0db2b4b
Tom Sherman <the.tomsherman@gmail.com>
2024-10-04 18:13:23 +0100

    Early return

--- packages/frontpage/app/(app)/moderation/page.tsx
+++ packages/frontpage/app/(app)/moderation/page.tsx
@@ -103,6 +103,10 @@ export default async function Moderation({
 }: {
   searchParams: { status: string };
 }) {
+  if (!(await isAdmin())) {
+    redirect("/");
+  }
+
   const status = (searchParams.status as StatusTypes) ?? null;
 
   const reportList = await getReports(status);
@@ -113,84 +117,80 @@ export default async function Moderation({
 
   const stats = await getModeratorReportStats();
 
-  if (await isAdmin()) {
-    return (
-      <>
-        <Card className="mb-4">
-          <CardHeader className="flex justify-evenly text-center">
-            <CardTitle className="text-2xl font-bold text-blue-300">
-              Moderation Dashboard
-            </CardTitle>
-            <CardDescription className="text-gray-300">
-              Review and take action on reported content
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="flex flex-wrap">
-            <Link href="/moderation" className="flex-1">
-              <Card className="text-center">
-                <CardHeader className="pb-2">
-                  <CardTitle className="text-sm font-medium text-gray-400">
-                    Total Reports
-                  </CardTitle>
-                </CardHeader>
-                <CardContent>
-                  <div className="text-2xl font-bold text-blue-300">
-                    {stats.total}
-                  </div>
-                </CardContent>
-              </Card>
-            </Link>
-            <Link href="/moderation?status=pending" className="flex-1">
-              <Card className="text-center">
-                <CardHeader className="pb-2">
-                  <CardTitle className="text-sm font-medium text-gray-400">
-                    Pending
-                  </CardTitle>
-                </CardHeader>
-                <CardContent>
-                  <div className="text-2xl font-bold text-yellow-300">
-                    {stats.pending}
-                  </div>
-                </CardContent>
-              </Card>
-            </Link>
-            <Link href="/moderation?status=accepted" className="flex-1">
-              <Card className="text-center">
-                <CardHeader className="pb-2">
-                  <CardTitle className="text-sm font-medium text-gray-400">
-                    Accepted
-                  </CardTitle>
-                </CardHeader>
-                <CardContent>
-                  <div className="text-2xl font-bold text-green-300">
-                    {stats.accepted}
-                  </div>
-                </CardContent>
-              </Card>
-            </Link>
+  return (
+    <>
+      <Card className="mb-4">
+        <CardHeader className="flex justify-evenly text-center">
+          <CardTitle className="text-2xl font-bold text-blue-300">
+            Moderation Dashboard
+          </CardTitle>
+          <CardDescription className="text-gray-300">
+            Review and take action on reported content
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="flex flex-wrap">
+          <Link href="/moderation" className="flex-1">
+            <Card className="text-center">
+              <CardHeader className="pb-2">
+                <CardTitle className="text-sm font-medium text-gray-400">
+                  Total Reports
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="text-2xl font-bold text-blue-300">
+                  {stats.total}
+                </div>
+              </CardContent>
+            </Card>
+          </Link>
+          <Link href="/moderation?status=pending" className="flex-1">
+            <Card className="text-center">
+              <CardHeader className="pb-2">
+                <CardTitle className="text-sm font-medium text-gray-400">
+                  Pending
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="text-2xl font-bold text-yellow-300">
+                  {stats.pending}
+                </div>
+              </CardContent>
+            </Card>
+          </Link>
+          <Link href="/moderation?status=accepted" className="flex-1">
+            <Card className="text-center">
+              <CardHeader className="pb-2">
+                <CardTitle className="text-sm font-medium text-gray-400">
+                  Accepted
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="text-2xl font-bold text-green-300">
+                  {stats.accepted}
+                </div>
+              </CardContent>
+            </Card>
+          </Link>
 
-            <Link href="/moderation?status=rejected" className="flex-1">
-              <Card className="text-center">
-                <CardHeader className="pb-2">
-                  <CardTitle className="text-sm font-medium text-gray-400">
-                    Rejected
-                  </CardTitle>
-                </CardHeader>
-                <CardContent>
-                  <div className="text-2xl font-bold text-red-300">
-                    {stats.rejected}
-                  </div>
-                </CardContent>
-              </Card>
-            </Link>
-          </CardContent>
-        </Card>
-        {reports.map((report) => (
-          <ReportCard key={report.id} report={report} />
-        ))}
-      </>
-    );
-  } else {
-    redirect("/");
-  }
+          <Link href="/moderation?status=rejected" className="flex-1">
+            <Card className="text-center">
+              <CardHeader className="pb-2">
+                <CardTitle className="text-sm font-medium text-gray-400">
+                  Rejected
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="text-2xl font-bold text-red-300">
+                  {stats.rejected}
+                </div>
+              </CardContent>
+            </Card>
+          </Link>
+        </CardContent>
+      </Card>
+      {reports.map((report) => (
+        <ReportCard key={report.id} report={report} />
+      ))}
+    </>
+  );
 }
