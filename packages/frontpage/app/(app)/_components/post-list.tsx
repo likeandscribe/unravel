df5866fa0303e78644ac804f25c1113d9f2a4771
WillCorrigan <wcorrigan90@gmail.com>
2024-10-09 13:55:26 +0100

    get types working, clean up return of post list

--- packages/frontpage/app/(app)/_components/post-list.tsx
+++ packages/frontpage/app/(app)/_components/post-list.tsx
@@ -18,15 +18,14 @@ export function PostList() {
       }
     },
   });
-  const { data, size, setSize } = useSWRInfinite(
+  const { data, size, setSize } = useSWRInfinite<Page>(
     (_, previousPageData) => {
-      if (previousPageData && !previousPageData.postCards.length) return null;
       return ["posts", previousPageData?.nextCursor ?? 0] as [string, number];
     },
     async (cursor) => {
       const data = await getMorePostsAction({
         limit: 10,
-        nextCursor: cursor[1],
+        nextCursor: cursor[1] as number,
       });
       return data;
     },
@@ -36,23 +35,20 @@ export function PostList() {
   return (
     <div className="space-y-6">
       {data?.map((page, indx) => {
-        const inner = <Suspense>{page.postCards}</Suspense>;
-        if (indx === data.length - 1) {
-          return (
-            <Fragment key={page.nextCursor}>
-              {inner}
-              {page.postCards.length === 0 ? (
+        return (
+          <Fragment key={page.nextCursor}>
+            <Suspense>{page.postCards}</Suspense>
+            {indx === data.length - 1 ? (
+              page.postCards.length === 0 ? (
                 <p className="text-center text-gray-400">No posts remaining</p>
               ) : (
                 <p ref={inViewRef} className="text-center text-gray-400">
                   Loading more posts
                 </p>
-              )}
-            </Fragment>
-          );
-        } else {
-          return <Fragment key={page.nextCursor}>{inner}</Fragment>;
-        }
+              )
+            ) : null}
+          </Fragment>
+        );
       })}
     </div>
   );
