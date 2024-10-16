6ffc4f3bcd1fcf9af73bccea82fdf18396e1468e
Tom Sherman <the.tomsherman@gmail.com>
2024-10-16 22:15:14 +0100

    More seo

--- packages/frontpage/app/(app)/profile/[user]/page.tsx
+++ packages/frontpage/app/(app)/profile/[user]/page.tsx
@@ -14,14 +14,41 @@ import { getBlueskyProfile } from "@/lib/data/user";
 import { getUserComments } from "@/lib/data/db/comment";
 import { Comment } from "../../post/[postAuthor]/[postRkey]/_lib/comment";
 import { Suspense } from "react";
-import { getDidFromHandleOrDid } from "@/lib/data/atproto/identity";
+import {
+  getDidFromHandleOrDid,
+  getVerifiedHandle,
+} from "@/lib/data/atproto/identity";
 import { EllipsisDropdown } from "../../_components/ellipsis-dropdown";
 import { ReportDialogDropdownButton } from "../../_components/report-dialog";
 import { reportUserAction } from "@/lib/components/user-hover-card";
+import { Metadata } from "next";
 
 type Params = {
   user: string;
 };
+
+export async function generateMetadata(props: {
+  params: Promise<Params>;
+}): Promise<Metadata> {
+  const params = await props.params;
+  const did = await getDidFromHandleOrDid(params.user);
+  if (!did) {
+    notFound();
+  }
+  const [handle, profile] = await Promise.all([
+    getVerifiedHandle(did),
+    getBlueskyProfile(did),
+  ]);
+  return {
+    title: `@${handle} on Frontpage`,
+    openGraph: {
+      title: `@${handle}`,
+      description: `@${handle} profile on Frontpage`,
+      type: "profile",
+      images: profile?.avatar,
+    },
+  };
+}
 
 export default async function Profile(props: { params: Promise<Params> }) {
   const params = await props.params;
