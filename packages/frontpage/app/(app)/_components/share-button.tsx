dfdc2e0e3bb0e2493847034d4b883a65eb333e17
Tom Sherman <the.tomsherman@gmail.com>
2024-10-18 10:17:56 +0100

    Use web share API on mobile

--- packages/frontpage/app/(app)/_components/share-button.tsx
+++ packages/frontpage/app/(app)/_components/share-button.tsx
@@ -2,12 +2,30 @@
 
 import { DropdownMenuItem } from "@/lib/components/ui/dropdown-menu";
 import { toast } from "@/lib/components/ui/use-toast";
-import { Share2Icon } from "@radix-ui/react-icons";
+import { Share1Icon, Link1Icon } from "@radix-ui/react-icons";
+
+const isMobile =
+  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent,
+  );
 
 export function ShareDropdownButton({ path }: { path: string }) {
   return (
     <DropdownMenuItem
       onSelect={() => {
+        const data: ShareData = {
+          title: "Frontpage",
+          url: new URL(path, location.href).toString(),
+        };
+
+        try {
+          if (isMobile && navigator.canShare(data)) {
+            void navigator.share(data);
+            return;
+          }
+        } catch (_) {
+          // Ignore
+        }
         void navigator.clipboard
           .writeText(new URL(path, location.href).toString())
           .then(() => {
@@ -18,7 +36,17 @@ export function ShareDropdownButton({ path }: { path: string }) {
           });
       }}
     >
-      <Share2Icon className="mr-2" /> Share
+      {isMobile ? (
+        <>
+          <Share1Icon className="mr-2" />
+          Share link
+        </>
+      ) : (
+        <>
+          <Link1Icon className="mr-2" />
+          Copy link to clipboard
+        </>
+      )}
     </DropdownMenuItem>
   );
 }
